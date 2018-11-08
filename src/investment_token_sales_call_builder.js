272c39c09964cf2ed4807532108dbd4ef141576d
Dmitry Sviridenko <letehaha@gmail.com>
2018-11-08 18:43:29 +0200

    Add new queries to investment_token_sales_call_builder

--- src/investment_token_sales_call_builder.js
+++ src/investment_token_sales_call_builder.js
@@ -19,84 +19,50 @@ export class InvestmentTokenSalesCallBuilder extends CallBuilder {
    * @param {string} asset For example: `USD`
    * @returns {InvestmentTokenSalesCallBuilder}
    */
-  forBaseAsset(asset) {
-    this.url.addQuery('base_asset', asset);
-    return this;
-  }
+    forBaseAsset(asset) {
+        this.url.addQuery('base_asset', asset);
+        return this;
+    }
 
-  /**
-   * Provides information on a single sale.
-   * @param {string} id Sale ID
-   * @returns {InvestmentTokenSalesCallBuilder}
-   */
+    /**
+     * Provides information on a single sale.
+     * @param {string} id Sale ID
+     * @returns {InvestmentTokenSalesCallBuilder}
+     */
 
-  sale(id) {
-    this.filter.push(['investment_token_sales', id.toString()]);
-    return this;
-  }
+    sale(id) {
+        this.filter.push(['investment_token_sales', id.toString()]);
+        return this;
+    }
 
-  /**
-   * Filters sales by owner
-   * @param {string} owner For example: `GDRYPVZ63SR7V2G46GKRGABJD3XPDNWQ4B4PQPJBTTDUEAKH5ZECPTSN`
-   * @returns {InvestmentTokenSalesCallBuilder}
-   */
+    /**
+     * Filters sales by owner
+     * @param {string} owner For example: `GDRYPVZ63SR7V2G46GKRGABJD3XPDNWQ4B4PQPJBTTDUEAKH5ZECPTSN`
+     * @returns {InvestmentTokenSalesCallBuilder}
+     */
 
-  forOwner(owner) {
-    this.url.addQuery('owner', owner);
-    return this;
-  }
+    forOwner(owner) {
+        this.url.addQuery('owner', owner);
+        return this;
+    }
 
-  /**
+      /**
    * Filters sales by open state
    * @param {boolean} openOnly
    * @return {InvestmentTokenSalesCallBuilder}
    */
-  openOnly(openOnly = true) {
-    this.url.addQuery('open_only', openOnly);
-    return this;
-  }
+      openOnly(openOnly = true) {
+        this.url.addQuery('open_only', openOnly);
+        return this;
+      }
 
-  /**
-   * Filter sales which will start soon.
-   * @param {boolean} upcoming
-   * @return {InvestmentTokenSalesCallBuilder}
-   */
-  upcoming(upcoming = true) {
-    this.url.addQuery('upcoming', upcoming);
-    return this;
-  }
-
-  /**
+      /**
    * Filters sales by name
    * @param {string} name For example: `awesome sale`
    * @returns {InvestmentTokenSalesCallBuilder}
    */
   forName(name) {
     this.url.addQuery('name', name);
-    return this;
-  }
-
-  /**
-   * Filter sales in which the current сap exceeds
-   * the specified percentage `bound` of the soft cap.
-   * @param {Number} bound - percent value from 0 to 100
-   */
-  currentSoftCapsRatio(bound = 0) {
-    if (bound < 0 || bound > 100) {
-      throw new Error("bound value is out of range 0 <= x <= 100");
-    }
-
-    this.url.addQuery('current_soft_caps_ratio', bound);
-    return this;
-  }
-
-  /**
-   * Filter sales in which the current сap exceeds `collectedValueBound`.
-   * @param {number} collectedValueBound - lower bound of the current cap.
-   * @returns {InvestmentTokenSalesCallBuilder}
-   */
-  withCollectedValueBound(collectedValueBound = 0) {
-    this.url.addQuery('collected_value_bound', collectedValueBound);
     return this;
   }
 
@@ -122,7 +88,7 @@ export class InvestmentTokenSalesCallBuilder extends CallBuilder {
    * Sort sales in order when sale with
    * the closest end time will be first.
    */
-  sortByEndType() {
+  sortByEndTime() {
     this.url.addQuery('sort_by', 2);
     return this;
   }
@@ -133,6 +99,42 @@ export class InvestmentTokenSalesCallBuilder extends CallBuilder {
    */
   sortByPopularity() {
     this.url.addQuery('sort_by', 3);
+    return this;
+  }
+
+  /**
+   * Sort sales in the order when a sale with latest date
+   * of creation will be the first.
+   */
+  sortByStartTime() {
+    this.url.addQuery('sort_by', 4);
+    return this;
+  }
+
+  /**
+   * Sort sales in the order when a sale with latest date
+   * of trading start will be the first.
+   */
+  sortByTradingStartDate() {
+    this.url.addQuery('sort_by', 5);
+    return this;
+  }
+
+  /**
+   * Sort sales in the order when a sale with latest date
+   * of settlements start will be the first.
+   */
+  sortBySettlementsStartDate() {
+    this.url.addQuery('sort_by', 6);
+    return this;
+  }
+
+  /**
+   * Sort sales in the order when a sale with latest date
+   * of settlements end will be the first.
+   */
+  sortBySettlementsEndDate() {
+    this.url.addQuery('sort_by', 7);
     return this;
   }
   
